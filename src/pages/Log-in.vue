60a778bf89f0e19fc1382e06cca22bca6b3259de
njorquera98 <njorquera98.dev@gmail.com>
2023-11-18 21:23:56 -0300

    Feat: New page log-in

---
+++ src/pages/Log-in.vue
@@ -0,0 +1,28 @@
+<template>
+  <q-page>
+    <div class="fixed-center">
+      <div class="q-gutter-y-md column" style="max-width: 300px">
+        <q-input v-model="text" label="Usuario">
+          <template v-slot:prepend>
+            <q-icon name="person" />
+          </template>
+        </q-input>
+      </div>
+      <div class="q-gutter-y-md column" style="max-width: 300px">
+        <q-input v-model="password" label="Contraseña">
+          <template v-slot:prepend>
+            <q-icon name="lock" />
+          </template>
+        </q-input>
+      </div>
+      <q-btn class="center" color="primary" label="Primary" to="/"/>
+    </div>
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  //  name: 'Index'
+})
+</script>
